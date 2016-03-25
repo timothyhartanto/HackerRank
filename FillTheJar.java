4ac67a13abf7b61bd1085a235a979a2638f93751
timothyhartanto <hartantotimothy@gmail.com>
2016-03-25 21:15:20 +0700

    Update FillTheJar.java

--- FillTheJar.java
+++ FillTheJar.java
@@ -12,19 +12,11 @@ public class FillTheJar {
         double M = Double.parseDouble(split[1]);
         double result = 0;
 
-//        double[] a = new double[(int) M];
-//        double[] b = new double[(int) M];
-//        double[] k = new double[(int) M];
-        
         for(int i = 0; i < M; i ++){
             read = br.readLine();
             split = read.split(" ");
-//            a[i] = Integer.parseInt(split[0]);
-//            b[i] = Integer.parseInt(split[1]);
-//            k[i] = Integer.parseInt(split[2]);
-            
+
             result += ((Double.parseDouble(split[1]) - Double.parseDouble(split[0]) + 1) * Double.parseDouble(split[2]));
-            //result += ((b[i] - a[i] + 1) * k[i]);
          }
         
         result = result / N;
